bd27a2366da4a9eadd52b9c566caafd813b629e2
Lindsay <lindsay98@live.nl>
2024-05-08 15:06:18 +0200

    Changes 08/05/2024
    added skin to enemy
    upgraded level layout

---
+++ MyFirstGame/Assets/CameraScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{   
+    //target dat word gevolg door camera
+    [SerializeField] private Transform target;
+    //hoe snel de camera volgt 
+    [SerializeField]
+    [Range(0.01f, 1f)]
+    private float smoothSpeed = 0.125f;
+    //verschil tussen target en camera
+    [SerializeField] private Vector3 offset;
+    //standaard geen verschil tussen target en camrera
+    private Vector3 velocity = Vector3.zero;
+    // Start is called before the first frame update
+
+    private void LateUpdate()
+    {
+        //waar de camera naar toe moet
+        Vector3 desiredPosition = target.position + offset;
+        //bewegen van de camera
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
